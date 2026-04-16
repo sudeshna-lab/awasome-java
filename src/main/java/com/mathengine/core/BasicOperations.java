c569194dfd28702a6c2c3973bb24dc0d4f2562fb
sudeshna-lab <62993714+sudeshna-lab@users.noreply.github.com>
2026-04-16 18:48:39 +0530

    Create BasicOperations class for core operations

---
+++ src/main/java/com/mathengine/core/BasicOperations.java
@@ -0,0 +1,20 @@
+package com.mathengine.core;
+
+public class BasicOperations {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) throw new ArithmeticException("Cannot divide by zero");
+        return (double) a / b;
+    }
+}
